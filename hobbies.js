3611360382dc3af27340b6e68fc53c772eac2037
Lucia994 <l.summa94@gmail.com>
2025-06-25 21:13:27 +0200

    Create userName and userHobbies functions

--- hobbies.js
+++ hobbies.js
@@ -1,1 +1,9 @@
 console.log('Hi node.js');
+// 2 - hobbies.js
+// Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire un array di stringhe contenenti gli hobby.
+// Esporta la funzione dal file.
+
+function userHobbies (hobbyOne, hobbyTwo, hobbyThree) { return ["Leggere", "Ascoltare la musica", "Visitare musei"]
+}
+
+module.export = userHobbies; 
--- names.js
+++ names.js
@@ -1,1 +1,13 @@
 console.log('Hi node.js');
+// 1 - names.js
+// Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+// Esporta la funzione dal file. 
+
+function userName(firstName, lastName) {
+    return {
+        firstName: "Elliot",
+        lastName: "Reid",
+    }
+}
+
+module.exports = userName;
